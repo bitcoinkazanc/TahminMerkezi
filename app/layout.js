760dff2a948a04cb3e022703fe40e9ca7574843f
bitcoinkazanc <bitcoinkazanc@gmail.com>
2026-08-19 20:42:17 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import Header from "../components/Header";
 import BottomNav from "../components/BottomNav";
 
@@ -11,10 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="tr">
       <head>
-        <script
+        <Script
           src="https://telegram.org/js/telegram-web-app.js"
-          async
-        ></script>
+          strategy="beforeInteractive"
+        />
       </head>
 
       <body>
