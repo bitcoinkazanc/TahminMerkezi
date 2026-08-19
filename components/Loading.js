cc873220cd5b037afe9d44b72959d7fd66dd05d7
bitcoinkazanc <bitcoinkazanc@gmail.com>
2026-08-19 18:22:09 +0300

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -1,1 +1,11 @@
- 
+export default function Loading({ text = "Yükleniyor..." }) {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner" />
+
+      <p className="loading-text">
+        {text}
+      </p>
+    </div>
+  );
+}
